892e38f6a93a569a7c057751f0c6d9939c77afbf
ChishuiLi <1192537447@qq.com>
2022-03-09 22:44:17 +0800

    完成problem数据的存储，还没有完成展示

---
+++ src/pages/Problem.vue
@@ -0,0 +1,76 @@
+<template>
+<div>
+  <Header :title="title"/>
+  <div class="center">
+    <form role="form">
+      题目编号：<input type="text" v-model="problem.num">
+      <div class="form-group">
+        <label>题目</label>
+        <input type="text" class="form-control" v-model="problem.text"/>
+      </div>
+      <div class="form-group">
+        <label>选项A</label>
+        <input type="text" class="form-control" v-model="problem.answer.A"/>
+      </div>
+      <div class="form-group">
+        <label>选项B</label>
+        <input type="text" class="form-control" v-model="problem.answer.B"/>
+      </div>
+      <div class="form-group">
+        <label>选项C</label>
+        <input type="text" class="form-control" v-model="problem.answer.C"/>
+      </div>
+      <div class="form-group">
+        <label>选项D</label>
+        <input type="text" class="form-control" v-model="problem.answer.D"/>
+      </div>
+      正确选项:<input type="text" v-model="problem.rightAnswer">
+      <div class="checkbox">
+        <label><input type="checkbox" />Check me out</label>
+      </div>
+      <button type="submit" class="btn btn-default" @click="setPro">确定</button>
+    </form>
+  </div>
+</div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import {mapActions} from 'vuex'
+export default {
+  name: "Problem",
+  components:{
+    Header
+  },
+  data(){
+    return{
+      title:'题卷',
+      problem:{
+        num:'', //题目编号
+        text:'', //题目内容
+        answer:{ //设置选项
+          A:'',
+          B:'',
+          C:'',
+          D:''
+        },
+        rightAnswer:'', //正确答案选项
+      }
+    }
+  },
+  methods:{
+    ...mapActions(['setProblem']),
+    setPro(){
+      this.setProblem(this.problem)
+    }
+  },
+
+}
+</script>
+
+<style scoped>
+.center{
+  background-color: #e6fffc;
+  width: 100%;
+}
+</style>
---
+++ src/pages/showProblem.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  showProblem
+</div>
+</template>
+
+<script>
+export default {
+  name: "showProblem"
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
